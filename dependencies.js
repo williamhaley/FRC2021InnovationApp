e43af647866ede0043a7871598a2b8a2357f405f
Will <willhy@gmail.com>
2021-01-22 19:55:24 -0600

    Overscroll. Improve caching. Startup. Icons.

--- dependencies.js
+++ dependencies.js
@@ -1,16 +1,27 @@
+const version = 'v1.0.15';
+
+console.log(`dependencies:version ${version}`);
+
 (() => {
   function registerServiceWorker() {
-    console.log('register service worker');
+    console.log('dependencies:registerServiceWorker');
     // Register the service worker if available.
     navigator.serviceWorker.register('./service-worker.js', { scope: '/' }).then(function(registration) {
-      console.log('Successfully registered service worker', registration);
-    }).catch(function(err) {
-      console.warn('Error whilst registering service worker', err);
+      if (registration.installing) {
+        console.log('dependencies:registration installing');
+      } else if (registration.waiting) {
+        console.log('dependencies:registration waiting');
+      } else if (registration.active) {
+        console.log('dependencies:registration active');
+      }
+    }).catch(function(error) {
+      console.log(`Registration failed with ${error}`);
     });
   }
 
   function updateServiceWorker(registration) {
-    console.log('update service worker', registration);
+    console.log('dependencies:updateServiceWorker', registration);
+    registration.update();
   }
 
   window.addEventListener('appinstalled', (event) => {
@@ -18,63 +29,49 @@
   });
 
   if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.getRegistrations().then(registrations => {
-      if (registrations.length === 0) {
-        registerServiceWorker();
-      } else {
-        updateServiceWorker(registrations[0]);
-      }
-    }).catch(err => {
-      console.log('error getting current registrations', err);
-    });
+    registerServiceWorker();
+    // navigator.serviceWorker.getRegistrations().then(registrations => {
+    //   if (registrations.length === 0) {
+    //     registerServiceWorker();
+    //   } else {
+    //     updateServiceWorker(registrations[0]);
+    //   }
+    // }).catch(err => {
+    //   console.log('error getting current registrations', err);
+    // });
   } else {
-    console.log('serviceServer not available');
+    console.log('dependencies:serviceWorker not available');
   }
 
   // Fires before 'load' and can be used to inform aspects of state.
   window.addEventListener('DOMContentLoaded', () => {
-    let displayMode = 'browser tab';
+    let displayMode = 'unknown (probably web)';
     if (navigator.standalone) {
-      displayMode = 'standalone-ios';
+      displayMode = 'standalone';
     }
     if (window.matchMedia('(display-mode: standalone)').matches) {
-      displayMode = 'standalone';
-      isInstalled = true;
+      displayMode = 'standalone-media';
     }
-    // Log launch display mode to analytics
-    console.log('DISPLAY_MODE_LAUNCH:', displayMode);
-  });
+    console.log(`dependencies:displayMode ${displayMode}`);
 
-  window.addEventListener('DOMContentLoaded', () => {
-    window.addEventListener('online', function(e) {
-      // Resync data with server.
-      console.log("You are now online");
-      // Page.hideOfflineWarning();
-      // Arrivals.loadData();
+    window.addEventListener('online', () => {
+      console.log('dependencies:online status changed to online');
     }, false);
 
-    window.addEventListener('offline', function(e) {
-      // Queue up events for server.
-      console.log("You are now offline");
-      // Page.showOfflineWarning();
+    window.addEventListener('offline', () => {
+      console.log('dependencies:offline status changed to offline');
     }, false);
 
-    // Check if the user is connected. If so and they're on a map page, render it.
-    if (navigator.onLine && /.*map.*/.test(window.location.pathname)) {
-      console.log('started online');
+    // Different browsers treat this differently.
+    const isOnline = navigator.onLine;
+    console.log(`dependencies:isOnline ${isOnline}`);
 
+    if (isOnline && /.*map.*/.test(window.location.pathname)) {
       const s = document.createElement('script');
       const src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyCe9Kd-PQQX4pQrhmP76Imolo7Kvn2Ev3I&callback=initMap&libraries=&v=weekly`;
       s.setAttribute('src', src);
       document.body.appendChild(s);
-
-      // Arrivals.loadData();
-    } else {
-      console.log('started offline');
-      // Show offline message
-      // Page.showOfflineWarning();
     }
-
     // setInterval(() => {
     //   navigator.geolocation.getCurrentPosition((position) => {
     //     console.log(position.coords.latitude, position.coords.longitude);
@@ -83,16 +80,15 @@
     // }, 2000);
   });
 
-  window.addEventListener('load', () => {
-    console.log(`page loaded: ${window.location.pathname}`);
-    if (['/', '/index.html'].includes(window.location.pathname)) {
-      console.log('loading from splash page...');
-      setTimeout(() => {
-        window.location.href = '/home.html';
-      }, 2000);
-    }
-  });
+  window.addEventListener('load', async () => {
+    let $version = document.createElement('span');
+    $version.textContent = version;
+    document.querySelector('footer').appendChild($version);
 
+    const res = await fetch('/manifest.json');
+    await res.text();
+    console.log('dependencies:load test fetch complete');
+  });
 })();
 
 let map;
--- service-worker.js
+++ service-worker.js
@@ -1,38 +1,83 @@
-// Use a cacheName for cache versioning and busting resources.
-var cacheName = 'v1:static';
+console.log('service-worker:start');
 
-console.log('1');
+// Force loading a new version of the service-worker by changing this.
+const version = 'v1.0.5';
+
+self.addEventListener('error', function (error) {
+  console.log(error.filename, error.lineno, error.colno, error.message);
+});
+
+// Use a cacheName for cache versioning and busting resources.
+let cacheName = `${version}:static`;
+
+console.log(`service-worker:version ${version}`);
+console.log(`service-worker:cacheName: ${cacheName}`);
+
+const filesToCache = [
+  './',
+  './index.html',
+  './map.html',
+  './styles.css',
+  './dependencies.js',
+];
 
 self.addEventListener('install', function (event) {
-  console.log('installed service worker');
+  console.log('service-worker:install');
 
   // Cache offline static assets.
   event.waitUntil(
     caches.open(cacheName).then(function (cache) {
-      return cache.addAll([
-        './',
-        './styles.css',
-        './dependencies.js',
-      ]).then(function() {
+      return cache.addAll(filesToCache).then(function() {
+        console.log('service-worker:cacheAll complete');
         self.skipWaiting();
       });
     })
   );
 });
 
+// Remove old cache if any.
+self.addEventListener('activate', (event) => {
+  console.log('service-worker:activate');
+
+  event.waitUntil((async () => {
+    const cacheNames = await caches.keys();
+
+    await Promise.all(cacheNames.map(async (cacheName) => {
+      if (self.cacheName !== cacheName) {
+        console.log(`service:worker:activate [${cacheName}] clean item from cache`);
+        await caches.delete(cacheName);
+      }
+    }));
+  })());
+});
+
 // when the browser fetches a URL…
 self.addEventListener('fetch', function(event) {
-  console.log('fetch', event);
+  event.respondWith(caches.match(event.request).then(function(response) {
+    // caches.match() always resolves
+    // but in case of success response will have value
+    if (response !== undefined) {
+      console.log(`service-worker:fetch [${event.request.method}] ${event.request.url} (cached)`);
+      return response;
+    }
 
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      // Cached resource. Load the cache.
-      if (response) {
-        // retrieve from cache
-        return response;
-      }
-      // fetch as normal
-      return fetch(event.request);
-    })
-  );
+    return fetch(event.request).then(function (response) {
+      console.log(`service-worker:fetch [${event.request.method}] ${event.request.url} (not-cached)`);
+
+      // response may be used only once
+      // we need to save clone to put one copy in cache
+      // and serve second one
+      let responseClone = response.clone();
+
+      caches.open(cacheName).then(function (cache) {
+        cache.put(event.request, responseClone);
+      });
+      return response;
+    }).catch(function () {
+      console.log('service-worker:error');
+      // return caches.match('/sw-test/gallery/myLittleVader.jpg');
+    });
+  }));
 });
+
+console.log('service-worker:end');
